4681c91a403b8febec327ecc1be6d88172bb6b65
Adriana Lapuste <adriana_98lapuste@yahoo.com>
2018-07-07 13:35:51 +0100

    create user styles as scss

--- src/app/user/create-user/create-user.component.ts
+++ src/app/user/create-user/create-user.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.scss']
 })
 export class CreateUserComponent implements OnInit {
 
